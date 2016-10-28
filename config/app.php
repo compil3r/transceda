58978e7b55f3f6d5b4ede5f19b8465d3acd5f797
youniverseweb <youniverseweb@ps552006.dreamhostps.com>
2016-10-28 07:17:50 -0700

    servidor

--- config/app.php
+++ config/app.php
@@ -136,8 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
+        Illuminate\Html\HtmlServiceProvider::class,      
         Khill\Lavacharts\Laravel\LavachartsServiceProvider::class,
 
         /*
@@ -198,7 +197,6 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Html'      => Illuminate\Html\HtmlFacade::class,
         'Form'      => Illuminate\Html\FormFacade::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     
 
     ],
